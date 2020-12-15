40630f2f1d76156a616618758d2825cc8f5cc34c
Joseph M <jmedina1985@hotmail.com>
2020-12-14 21:52:43 -0800

    Structural change and starting About component

---
+++ src/components/AboutSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AboutSection = () => {
+  return(
+    <div>
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>your <span>dreams</span></h2>
+          </div>
+          <div className="hide">
+            <h2>come true.</h2>
+          </div>
+        </div>
+        <p>Contact</p>
+      </div>
+    </div>
+  )
+}
+
+export default AboutSection;
