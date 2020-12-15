0abc58eb2e57a8bb9fc99629bdfc1b046fd82a68
Joseph M <jmedina1985@hotmail.com>
2020-12-14 23:25:05 -0800

    Services component html

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,6 +5,7 @@ import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
+import home2 from '../img/home2.png'
 
 const ServicesSection = () => {
   return (
@@ -19,7 +20,31 @@ const ServicesSection = () => {
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <div className="card">
+            <div className="icon">
+              <img src={teamwork} alt="teamwork"/>
+              <h3>Teamwork</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={diaphragm} alt="diaphragm"/>
+              <h3>Diaphragm</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className="card">
+            <div className="icon">
+              <img src={money} alt="money"/>
+              <h3>Affordable</h3>
+            </div>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
         </div>
+      </div>
+      <div className="image">
+        <img src={home2} alt="home 2"/>
       </div>
     </div>
   )
