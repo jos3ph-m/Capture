d307c08aa4ba53b067ff4aef8a21ff2c6b0db456
Joseph M <jmedina1985@hotmail.com>
2020-12-15 23:37:09 -0800

    Scratch that, now the global styles are complete

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -25,6 +25,37 @@ button{
     background-color: #23d997;
     color: white;
   }
+  
+}
+
+h2{
+    font-weight: lighter;
+    font-size: 4rem;
+}
+
+h3{
+    color: white;
+}
+
+h4 {
+  font-weight: bold;
+
+}
+
+span {
+  font-weight: bold;
+  color: #23d997; 
+}
+
+a{
+  font-size: 1.1rem;
+}
+
+p{
+    padding: 3rem 0rem;
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
 }
 
 `
