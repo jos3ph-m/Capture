cc54237dba0bb1fddb2cbb57e551e9cbfe1decfd
Joseph M <jmedina1985@hotmail.com>
2020-12-15 23:32:55 -0800

    Global style complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// Global style
+import GlobalStyle from './components/GlobalStyle';
 //import pages
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />      
     </div>
   );
