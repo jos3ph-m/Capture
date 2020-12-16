44b782fbe372386e84354de911ac26dc056e300a
Joseph M <jmedina1985@hotmail.com>
2020-12-15 23:26:25 -0800

    Messing with styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import home1 from '../img/home1.png'
 
+import styled from 'styled-components';
+
 const AboutSection = () => {
   return(
-    <div>
+    <About>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -22,8 +24,18 @@ const AboutSection = () => {
       <div className="image">
         <img src={home1} alt="Man with a camera"/>
       </div>
-    </div>
+    </About>
   )
 }
+
+//Styled Component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
 
 export default AboutSection;
