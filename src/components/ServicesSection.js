c244f896e16065db485c816d2ce875d634a04682
Joseph M <jmedina1985@hotmail.com>
2020-12-16 21:56:05 -0800

    And now Card styles

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -17,34 +17,34 @@ const ServicesSection = () => {
       <Description>
         <h2>High <span>quality</span> services</h2>
         <Cards>
-          <div className="card">
+          <Card>
             <div className="icon">
               <img src={clock} alt="clock"/>
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="teamwork"/>
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="diaphragm"/>
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <img src={money} alt="money"/>
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
-          </div>
+          </Card>
         </Cards>
       </Description>
       <Image>
@@ -62,10 +62,19 @@ const Services = styled(About)`
     width: 70%;
     padding: 2rem 0 4rem 0rem;
   }
-`
+`;
+
 const Cards = styled.div`
   display: flex;
   flex-wrap: wrap;
-`
+`;
+
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon{
+    display: flex;
+    align-items: center;
+  }
+`;
 
 export default ServicesSection;
