2039ab82c9556ef29f4ed96ebf1f2323ceeb7700
Joseph M <jmedina1985@hotmail.com>
2020-12-16 21:46:37 -0800

    Using styles.js for general styles now

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import home1 from '../img/home1.png'
+import { About, Description, Image, Hide } from '../styles'
 
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 const AboutSection = () => {
   return(
@@ -29,35 +30,5 @@ const AboutSection = () => {
 }
 
 //Styled Component
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2{
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`
 
 export default AboutSection;
