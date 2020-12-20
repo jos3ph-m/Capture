c3f4b1cc890cf162fbc0629673dbb2acd31ba4b6
Joseph M <jmedina1985@hotmail.com>
2020-12-19 22:26:03 -0800

    Furthering the styles on FaqSection

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -43,6 +43,26 @@ const Faq = styled(About)`
   span{
     display: block;
   }
+  h2{
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line{
+    background: #cccccc;
+    height: 0.2rem;
+    margin: 2rem 0rem;
+    width: 100%;
+  }
+  .question{
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer{
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
 `;
 
 export default FaqSection;
