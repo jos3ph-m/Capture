7600c2415809bae93899a58e2247b1b3ab394468
Joseph M <jmedina1985@hotmail.com>
2020-12-20 23:55:58 -0800

    Nav basic styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const Nav = () => {
   return (
-    <nav>
+    <StyledNav>
       <h1><a href="#">Capture</a></h1>
       <ul>
         <li>
@@ -16,8 +16,15 @@ const Nav = () => {
           <a href="#">3. Contact Us</a>
         </li>
       </ul>
-    </nav>
+    </StyledNav>
   )
 }
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+`
 
 export default Nav;
