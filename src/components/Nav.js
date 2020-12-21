7e4b1e64172012b67af537f9d9b3bb09535103a1
Joseph M <jmedina1985@hotmail.com>
2020-12-20 23:54:00 -0800

    Nav pre-styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,20 @@ import styled from 'styled-components';
 
 const Nav = () => {
   return (
-
+    <nav>
+      <h1><a href="#">Capture</a></h1>
+      <ul>
+        <li>
+          <a href="#">1. About Us</a>
+        </li>
+        <li>
+          <a href="#">2. Our Work</a>
+        </li>
+        <li>
+          <a href="#">3. Contact Us</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
