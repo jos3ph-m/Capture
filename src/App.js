4da433c11d52e9391af188c6dd20563afbe74c5f
Joseph M <jmedina1985@hotmail.com>
2020-12-21 23:15:52 -0800

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 // Global style
 import GlobalStyle from './components/GlobalStyle';
-//import pages
+// Import pages
 import AboutUs from './pages/AboutUs';
 import ContactUs from './pages/ContactUs';
 import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+// Router
+import {Switch, Route} from 'react';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />  
-      <OurWork />  
-      <ContactUs />  
+      <Route path="/">
+        <AboutUs />
+      </Route>
+      <Route path="/work">
+        <OurWork />
+      </Route>
+      <Route path="/contact">
+        <ContactUs />
+      </Route>
     </div>
   );
 }
