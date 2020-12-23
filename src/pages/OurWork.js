1bd33809ea082ecb5a2ef2969220df190afd5b74
Joseph M <jmedina1985@hotmail.com>
2020-12-22 23:34:15 -0800

    Working on OurWork

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+//Images
+import athlete from '../img/athlete-small.png'
+import theracer from '../img/theracer-small.png'
+import goodtimes from '../img/goodtimes-small.png'
+
 
 const OurWork = () => {
   return(
