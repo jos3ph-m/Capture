d57bd0af3f00bb63c584c23da3a65cb0a5f461cb
Joseph M <jmedina1985@hotmail.com>
2020-12-23 23:09:04 -0800

    Basic setup for MovieDetail

---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
+
+const MovieDetail = () => {
+  <div>
+    <h1>MovieDetail</h1>
+  </div>
+}
+
+export default MovieDetail;
