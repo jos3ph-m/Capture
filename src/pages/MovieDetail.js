5185b9fdbb662fb29a91c6c0ec3c5250fa5b019e
Joseph M <jmedina1985@hotmail.com>
2020-12-23 23:33:35 -0800

    tracking movie and url

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
+import { MovieState } from '../movieState';
 
 const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  // useEffect
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
+    setMovie(currentMovie);
+  }, [movies, url]);
+
   return(
     <div>
       <h1>MovieDetail</h1>
