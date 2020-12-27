bc48b31f75c331426c6702e0785ea5d77b9a75f7
Joseph M <jmedina1985@hotmail.com>
2020-12-26 23:33:11 -0800

    Finishing up movie details

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -28,6 +28,9 @@ const MovieDetail = () => {
           <Award title={award.title} description={award.description} key={award.title}/>
         ))}
       </Awards>
+      <ImageDisplay>
+        <img src={movie.secondaryImg} alt="secondary image"/>
+      </ImageDisplay>
     </Details>
     )}
     </>
@@ -64,17 +67,38 @@ const Awards = styled.div`
 `
 
 const AwardStyle = styled.div`
-  
+  padding: 5rem;
+  h3{
+    font-size: 2rem;
+  }
+  .line{
+    width: 100%;
+    background: #23d997;
+    height: 0.5rem;
+    margin: 1rem 0rem;
+  }
+  p{
+    padding: 2rem 0rem;
+  }
+`
+
+const ImageDisplay = styled.div`
+  min-height: 50vh;
+  img{
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
 `
 
 // Award Component
 const Award = ({title,description}) => {
   return (
-    <div>
+    <AwardStyle>
       <h3>{title}</h3>
       <div className="line"></div>
       <p>{description}</p>
-    </div>
+    </AwardStyle>
   )
 }
 
