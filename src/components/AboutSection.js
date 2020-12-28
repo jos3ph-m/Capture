f5018da72c8991a4e6de782ce87a787119fc43e8
Joseph M <jmedina1985@hotmail.com>
2020-12-27 23:42:33 -0800

    Stagger added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -14,7 +14,7 @@ const AboutSection = () => {
 
   const container = {
     hidden: { x: 100 },
-    show: { x: 0 }
+    show: { x: 0, transition: { duration: 0.75, ease: "easeOut", staggerChildren: 0.5 } }
   }
 
   return(
@@ -22,13 +22,13 @@ const AboutSection = () => {
       <Description>
         <motion.div variants={container} initial="hidden" animate="show" className="title">
           <Hide>
-            <motion.h2 variants={titleAnim} initial="hidden" animate="show">We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>your <span>dreams</span></h2>
+            <motion.h2 variants={titleAnim}>your <span>dreams</span></motion.h2>
           </Hide>
           <Hide>
-            <h2>come true.</h2>
+            <motion.h2 variants={titleAnim}>come true.</motion.h2>
           </Hide>
         </motion.div>
         <p>Contact us for any photography or videography ideas that you have.  We have professionalswith amazing skills.</p>
