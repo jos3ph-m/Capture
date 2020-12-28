0b6329f0513439b3d626dbdbb375589a4b7c047b
Joseph M <jmedina1985@hotmail.com>
2020-12-27 23:24:27 -0800

    Starting with framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import home1 from '../img/home1.png'
 import { About, Description, Image, Hide } from '../styles'
+// Framer Motion
+import { motion } from 'framer-motion';
 
-// import styled from 'styled-components';
 
 const AboutSection = () => {
   return(
