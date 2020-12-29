f0382d543e230933cf6d1d40e600d779e2ce9452
Joseph M <jmedina1985@hotmail.com>
2020-12-28 22:30:07 -0800

    Removed all variants - actually this time

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -7,28 +7,18 @@ import { motion } from 'framer-motion';
 
 const AboutSection = () => {
 
-  const titleAnim = {
-    hidden: { opacity: 0 },
-    show: { opacity: 1, transition: { duration: 2 } },
-  }
-
-  const container = {
-    hidden: { x: 100 },
-    show: { x: 0, transition: { duration: 0.75, ease: "easeOut", staggerChildren: 0.5 } }
-  }
-
   return(
     <About>
       <Description>
-        <motion.div variants={container} initial="hidden" animate="show" className="title">
+        <motion.div>
           <Hide>
-            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim}>your <span>dreams</span></motion.h2>
+            <motion.h2>your <span>dreams</span></motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnim}>come true.</motion.h2>
+            <motion.h2>come true.</motion.h2>
           </Hide>
         </motion.div>
         <p>Contact us for any photography or videography ideas that you have.  We have professionalswith amazing skills.</p>
