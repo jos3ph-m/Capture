2f9aa3225da8dd4d887b2793601e286b7e3fbbe3
Joseph M <jmedina1985@hotmail.com>
2021-01-01 23:05:39 -0800

    Toggle component - 1

--- src/animation.js
+++ src/animation.js
@@ -14,7 +14,6 @@ export const pageAnimation = {
   },
   exit: {
     opacity: 0,
-    y:300,
     transition: {
       duration: 0.5,
     }
---
+++ src/components/Toggle.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const Toggle = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default Toggle;
