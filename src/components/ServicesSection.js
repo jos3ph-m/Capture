9e0e2d87f59ff1d57a5e20400c0a23fcceb11134
Joseph M <jmedina1985@hotmail.com>
2021-01-03 22:08:35 -0800

    Test

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -5,42 +5,48 @@ import clock from '../img/clock.svg';
 import diaphragm from '../img/diaphragm.svg';
 import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
-import home2 from '../img/home2.png'
+import home2 from '../img/home2.png';
 
 //styles
-import styled from "styled-components";
-import { About, Description, Image } from '../styles'
+import styled from 'styled-components';
+import { About, Description, Image } from '../styles';
+
+//Test
+import { useInView } from 'react-intersection-observer';
+import { useAnimation } from 'framer-motion';
 
 const ServicesSection = () => {
   return (
     <Services>
       <Description>
-        <h2>High <span>quality</span> services</h2>
+        <h2>
+          High <span>quality</span> services
+        </h2>
         <Cards>
           <Card>
             <div className="icon">
-              <img src={clock} alt="clock"/>
+              <img src={clock} alt="clock" />
               <h3>Efficient</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-              <img src={teamwork} alt="teamwork"/>
+              <img src={teamwork} alt="teamwork" />
               <h3>Teamwork</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-              <img src={diaphragm} alt="diaphragm"/>
+              <img src={diaphragm} alt="diaphragm" />
               <h3>Diaphragm</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
           </Card>
           <Card>
             <div className="icon">
-              <img src={money} alt="money"/>
+              <img src={money} alt="money" />
               <h3>Affordable</h3>
             </div>
             <p>Lorem ipsum dolor sit amet.</p>
@@ -48,17 +54,17 @@ const ServicesSection = () => {
         </Cards>
       </Description>
       <Image>
-        <img src={home2} alt="home 2"/>
+        <img src={home2} alt="home 2" />
       </Image>
     </Services>
-  )
-}
+  );
+};
 
 const Services = styled(About)`
   h2 {
     padding-bottom: 5rem;
   }
-  p{
+  p {
     width: 70%;
     padding: 2rem 0 4rem 0rem;
   }
@@ -71,7 +77,7 @@ const Cards = styled.div`
 
 const Card = styled.div`
   flex-basis: 20rem;
-  .icon{
+  .icon {
     display: flex;
     align-items: center;
     h3 {
