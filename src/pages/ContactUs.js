542de80d082b4b98a9062cbbea6a2083449bf3ab
Joseph M <jmedina1985@hotmail.com>
2021-01-05 14:01:53 -0800

    ContactUs page getting some work done

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,40 @@
 import React from 'react';
 //Animations
 import { motion } from 'framer-motion';
-import { pageAnimation } from '../animation';
+import { pageAnimation, titleAnim } from '../animation';
+import styled from 'styled-components';
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       exit="exit"
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       style={{ background: '#fff' }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default ContactUs;
