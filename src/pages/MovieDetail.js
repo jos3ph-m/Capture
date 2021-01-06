5f5947d52bcb543d1a0fcf9422085c07d3ab8aa4
Joseph M <jmedina1985@hotmail.com>
2021-01-05 23:32:50 -0800

    Mobile styling - 4

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -14,52 +14,61 @@ const MovieDetail = () => {
 
   // useEffect - when we use filter on an array we are left with an array
   useEffect(() => {
-    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
     setMovie(currentMovie[0]);
   }, [movies, url]);
 
-  return(
+  return (
     <>
-    {movie && (
-    <Details exit="exit" variants={pageAnimation} initial="hidden" animate="show">
-      <HeadLine>
-        <h2>{movie.title}</h2>
-        <img src={movie.mainImg} alt="movie"/>
-      </HeadLine>
-      <Awards>
-        {movie.awards.map((award) => (
-          <Award title={award.title} description={award.description} key={award.title}/>
-        ))}
-      </Awards>
-      <ImageDisplay>
-        <img src={movie.secondaryImg} alt="secondary image"/>
-      </ImageDisplay>
-    </Details>
-    )}
+      {movie && (
+        <Details
+          exit="exit"
+          variants={pageAnimation}
+          initial="hidden"
+          animate="show"
+        >
+          <HeadLine>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie" />
+          </HeadLine>
+          <Awards>
+            {movie.awards.map((award) => (
+              <Award
+                title={award.title}
+                description={award.description}
+                key={award.title}
+              />
+            ))}
+          </Awards>
+          <ImageDisplay>
+            <img src={movie.secondaryImg} alt="secondary image" />
+          </ImageDisplay>
+        </Details>
+      )}
     </>
   );
-}
+};
 
 const Details = styled(motion.div)`
   color: white;
-`
+`;
 
 const HeadLine = styled.div`
   min-height: 90vh;
   padding-top: 20vh;
   position: relative;
-  h2{
+  h2 {
     position: absolute;
     top: 10%;
     left: 50%;
     transform: translate(-50%, -10%);
   }
-  img{
+  img {
     width: 100%;
     height: 70vh;
     object-fit: cover;
   }
-`
+`;
 
 const Awards = styled.div`
   min-height: 80vh;
@@ -67,42 +76,46 @@ const Awards = styled.div`
   margin: 5rem 10rem;
   align-items: center;
   justify-content: space-around;
-`
+  @media (max-width: 1500px) {
+    display: block;
+    margin: 2rem 2rem;
+  }
+`;
 
 const AwardStyle = styled.div`
   padding: 5rem;
-  h3{
+  h3 {
     font-size: 2rem;
   }
-  .line{
+  .line {
     width: 100%;
     background: #23d997;
     height: 0.5rem;
     margin: 1rem 0rem;
   }
-  p{
+  p {
     padding: 2rem 0rem;
   }
-`
+`;
 
 const ImageDisplay = styled.div`
   min-height: 50vh;
-  img{
+  img {
     width: 100%;
     height: 100vh;
     object-fit: cover;
   }
-`
+`;
 
 // Award Component
-const Award = ({title,description}) => {
+const Award = ({ title, description }) => {
   return (
     <AwardStyle>
       <h3>{title}</h3>
       <div className="line"></div>
       <p>{description}</p>
     </AwardStyle>
-  )
-}
+  );
+};
 
 export default MovieDetail;
