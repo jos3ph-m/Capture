a8ffb8cafe2653d72efa3b835cb751184cc3eaf9
Joseph M <jmedina1985@hotmail.com>
2021-01-05 22:32:45 -0800

    sticky top nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,11 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <StyledNav>
-      <h1><a id="logo" href="#">Capture</a></h1>
+      <h1>
+        <a id="logo" href="#">
+          Capture
+        </a>
+      </h1>
       <ul>
         <li>
           <Link to="/">1. About Us</Link>
@@ -18,8 +22,8 @@ const Nav = () => {
         </li>
       </ul>
     </StyledNav>
-  )
-}
+  );
+};
 
 const StyledNav = styled.nav`
   min-height: 10vh;
@@ -29,7 +33,10 @@ const StyledNav = styled.nav`
   align-items: center;
   padding: 1rem 10rem;
   background-color: #282828;
-  a{
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  a {
     color: white;
     text-decoration: none;
   }
@@ -39,13 +46,13 @@ const StyledNav = styled.nav`
   }
   #logo {
     font-size: 1.5rem;
-    font-family: "Lobster", cursive;
+    font-family: 'Lobster', cursive;
     font-weight: lighter;
   }
-  li{
+  li {
     padding-left: 10rem;
     position: relative;
   }
-`
+`;
 
 export default Nav;
